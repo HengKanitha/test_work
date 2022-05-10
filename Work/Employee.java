736934f979f6abee6c0b724140b5c875c9c4d22b
HengKanitha <105195275+HengKanitha@users.noreply.github.com>
2022-05-10 11:45:46 +0700

    initial

---
+++ Work/Employee.java
@@ -0,0 +1,8 @@
+package Work;
+
+public  interface Employee {
+    void employ();
+    int id=1;
+    String name="Lin";
+    String gender="female";
+}
---
+++ Work/Work.java
@@ -0,0 +1,10 @@
+package Work;
+
+public class Work {
+   public int id=1;
+   public String name="SreyLin";
+   public String gender="Female";
+
+    }
+
+
